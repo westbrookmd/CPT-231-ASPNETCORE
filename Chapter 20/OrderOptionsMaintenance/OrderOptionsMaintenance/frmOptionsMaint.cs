546aaca192897b3579a5c444fe3e823971a3085a
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2022-01-11 15:53:16 -0500

    Generated DB Context
    Added the DB via the Nuget Package Manager Console.

---
+++ Chapter 20/OrderOptionsMaintenance/OrderOptionsMaintenance/frmOptionsMaint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderOptionsMaintenance
+{
+    public partial class frmOptionsMaint : Form
+    {
+        public frmOptionsMaint()
+        {
+            InitializeComponent();
+        }
+
+        private void frmOptionsMaint_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+
+            }
+        }
+
+        private bool IsValidData()
+        {
+            return Validator.IsPresent(txtSalesTax) &&
+                   Validator.IsDecimal(txtSalesTax) &&
+                   Validator.IsPresent(txtShipFirstBook) &&
+                   Validator.IsDecimal(txtShipFirstBook) &&
+                   Validator.IsPresent(txtShipAddlBook) &&
+                   Validator.IsDecimal(txtShipAddlBook);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
