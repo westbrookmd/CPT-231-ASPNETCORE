fd06f87697d565d3995e2aaea10db624327e62fd
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2022-01-28 08:13:32 -0500

    Module  2 Start Files
    Adds the files required for the first assignment of module 2.

---
+++ Module 2/Chapter 21/OrderOptionsMaintenance/OrderOptionsMaintenance/Models/OrderOptionDB.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderOptionsMaintenance.Models
+{
+    public static class OrderOptionDB
+    {
+        public static OrderOption GetOrderOptions()
+        {
+            return null;
+        }
+
+        public static bool UpdateOrderOption(OrderOption oldOptions, OrderOption newOptions)
+        {
+            return false;        
+        }
+    }
+}
