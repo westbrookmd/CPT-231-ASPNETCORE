c7348e0709774c092d5702bdf2b2e2cc73f574da
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2022-01-28 11:52:55 -0500

    Comments for DB access
    Adds comments for the db access class.

--- Module 2/Chapter 21/OrderOptionsMaintenance/OrderOptionsMaintenance/Models/OrderOptionDB.cs
+++ Module 2/Chapter 21/OrderOptionsMaintenance/OrderOptionsMaintenance/Models/OrderOptionDB.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Data.SqlClient;
+﻿/* 
+* Marshall Westbrook
+* CPT-231-S14
+* M1-2 Assignment
+* Spring 2022
+* A project that demonstrates how to use ADO.NET
+*/
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace OrderOptionsMaintenance.Models
@@ -7,19 +14,22 @@ namespace OrderOptionsMaintenance.Models
     {
         public static OrderOption GetOrderOptions()
         {
+            // create a default orderoption
             OrderOption orderOption = new OrderOption();
+            // select statement that doesn't filter by rows
             string getOrderStatement =
                 "SELECT SalesTaxRate, FirstBookShipCharge, AdditionalBookShipCharge " +
                 "FROM OrderOptions";
             using SqlConnection connection = new SqlConnection(MMABooksDB.ConnectionString);
             using SqlCommand command = new SqlCommand(
                 getOrderStatement, connection);
+            // creating parameters to create proper mappings
             command.Parameters.AddWithValue("@SalesTax", orderOption.SalesTaxRate);
             command.Parameters.AddWithValue("@FirstBookShipCharge", orderOption.FirstBookShipCharge);
             command.Parameters.AddWithValue("@AdditionalBookShipCharge", orderOption.AdditionalBookShipCharge);
 
             connection.Open();
-
+            // only returning a single result (doing this here instead of in a WHERE clause
             using SqlDataReader reader = command.ExecuteReader(
                 CommandBehavior.SingleResult & CommandBehavior.CloseConnection);
             if (reader.Read())
@@ -31,6 +41,8 @@ namespace OrderOptionsMaintenance.Models
                     AdditionalBookShipCharge = (decimal)reader["AdditionalBookShipCharge"]
                 };
             }
+            // close the connection as a safe meassure.
+            // I think this would automatically be closed due to being out of scope
             connection.Close();
             return orderOption;
         }
@@ -47,6 +59,7 @@ namespace OrderOptionsMaintenance.Models
             using SqlConnection connection = new SqlConnection(MMABooksDB.ConnectionString);
             using SqlCommand command = new SqlCommand(
                 updateStatement, connection);
+            // creating the mappings to our OrderOption
             command.Parameters.AddWithValue("@NewSalesTaxRate", newOptions.SalesTaxRate);
             command.Parameters.AddWithValue("@NewFirstBookShipCharge", newOptions.FirstBookShipCharge);
             command.Parameters.AddWithValue("@NewAdditionalBookShipCharge", newOptions.AdditionalBookShipCharge);
@@ -55,6 +68,7 @@ namespace OrderOptionsMaintenance.Models
             command.Parameters.AddWithValue("@OldAdditionalBookShipCharge", oldOptions.AdditionalBookShipCharge);
             connection.Open();
 
+            // capture our result and return whether it was successful or not
             int rowsreturned = command.ExecuteNonQuery();
             connection.Close();
             return (rowsreturned > 0);
