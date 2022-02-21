c47f4aca9d744e5bca3c19613a6b6d67d0f810ff
westbrookmd <westbrookmd1@gmail.com>
2022-02-21 14:52:41 -0500

    U2M5 Example External CSS Usage

---
+++ Unit 2/Module 5/InClassU2M5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InClassU2M5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
