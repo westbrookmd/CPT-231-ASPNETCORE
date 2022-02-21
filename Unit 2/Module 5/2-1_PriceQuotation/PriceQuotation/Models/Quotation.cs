6beb6c7cacb062811459445b466ecafef0bfad9d
westbrookmd <westbrookmd1@gmail.com>
2022-02-21 14:25:34 -0500

    U2M5 Starting Point

---
+++ Unit 2/Module 5/2-1_PriceQuotation/PriceQuotation/Models/Quotation.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class Quotation
+    {
+        [Required(ErrorMessage = "Please enter a sale price.")]
+        [Range(0, 100000000, ErrorMessage = "Sale price must be greater than zero.")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0, 100, ErrorMessage = "Discount percent must be between 0 and 100.")]
+        public double? DiscountPercent { get; set; }
+
+        public double CalculateDiscount() {
+            if (Subtotal.HasValue && DiscountPercent.HasValue)
+            {
+                return Subtotal.Value * (DiscountPercent.Value / 100);  
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            if (Subtotal.HasValue)
+            {
+                double discount = CalculateDiscount();
+                return Subtotal.Value - discount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
