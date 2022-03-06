b19d26bdb149818d3781af52efad76a2568f71f7
westbrookmd <westbrookmd1@gmail.com>
2022-03-06 18:16:16 -0500

    M6 Assignment

---
+++ Unit 2/Module 6/M6Assignment/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using M6Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M6Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AirportContext _airportContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, AirportContext airportContext)
+        {
+            _logger = logger;
+            _airportContext = airportContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Airport> airports = _airportContext.Airports.OrderBy(
+                a => a.AirportID).ToList<Airport>();
+            return View(airports);
+        }
+    }
+}
