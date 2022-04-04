50930406a772d0b4e87212f33b64609cb30e3ce8
westbrookmd <westbrookmd1@gmail.com>
2022-04-04 13:59:53 -0400

    U3M9 Initial Commit

---
+++ Unit 3/Module 9/M9Assignment/Areas/Admin/Models/Airport.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace M9Assignment.Admin.Models
+{
+    public class Airport
+    {
+        public int AirportID { get; set; }
+        [Required(ErrorMessage = "You must enter the code of the airport.")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "You must enter the name of the airport.")]
+        public string Name { get; set; }
+    }
+}
---
+++ Unit 3/Module 9/M9Assignment/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using M9Assignment.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M9Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AirportContext _airportContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, AirportContext airportContext)
+        {
+            _logger = logger;
+            _airportContext = airportContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Airport()
+        {
+            List<Airport> airports = _airportContext.Airports.OrderBy(
+                a => a.AirportID).ToList<Airport>();
+            return View(airports);
+        }
+    }
+}
