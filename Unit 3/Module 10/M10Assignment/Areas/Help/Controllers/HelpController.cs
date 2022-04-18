06a14181c480028ee0d3c10d6a476de198ec7887
westbrookmd <westbrookmd1@gmail.com>
2022-04-17 23:04:40 -0400

    U3M10 Init

---
+++ Unit 3/Module 10/M10Assignment/Areas/Help/Controllers/HelpController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace M10Assignment.Areas.Help.Controllers
+{
+    [Area("Help")]
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Unit 3/Module 10/M10Assignment/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using M10Assignment.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M10Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AirportContext _airportContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, AirportContext airportContext)
+        {
+            _logger = logger;
+            _airportContext = airportContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Airport()
+        {
+            List<Airport> airports = _airportContext.Airports.OrderBy(
+                a => a.AirportID).ToList<Airport>();
+            return View(airports);
+        }
+    }
+}
