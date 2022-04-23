e7bb00dc68bf7ad65a5c3dcbadd174d52037caaa
westbrookmd <westbrookmd1@gmail.com>
2022-04-22 23:27:58 -0400

    U3M11 InClass

---
+++ Unit 3/Module 11/InClass_NFLTeams/NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            NFLSession session = new NFLSession(HttpContext.Session);
+            TeamListViewModel model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                UserName = session.GetName()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Change(TeamListViewModel model)
+        {
+            NFLSession session = new NFLSession(HttpContext.Session);            
+            session.SetName(model.UserName);           
+            return RedirectToAction("Index", "Home", new {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+    }
+}
