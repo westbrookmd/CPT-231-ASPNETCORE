84c05dec5951bb7c29b7e8f5798596c13a9b6e6f
westbrookmd <westbrookmd1@gmail.com>
2022-04-22 23:34:31 -0400

    U3M11 Working Maze

---
+++ Unit 3/Module 11/M11Assignment/M11Assignment/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using M11Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M11Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RoomContext roomContext { get; set; }
+        public HomeController(RoomContext roomContext)
+        {
+            this.roomContext = roomContext;
+        }
+
+        public IActionResult Index(Room room)
+        {
+            if(room.Description == null)
+            {
+                room = roomContext.Rooms.Where(r => r.X == 0 && r.Y == 0).FirstOrDefault();
+                ViewBag.Room = room;
+            }
+            Room northRoom = roomContext.Rooms.Where(r => r.X == room.X && r.Y == room.Y + 1).FirstOrDefault();
+            ViewBag.North = northRoom != null;
+            if(northRoom != null)
+            {
+                ViewBag.NorthID = northRoom.RoomId;
+            }
+            
+            Room southRoom = roomContext.Rooms.Where(r => r.X == room.X && r.Y == room.Y - 1).FirstOrDefault();
+            ViewBag.South = southRoom != null;
+            if(southRoom != null)
+            {
+                ViewBag.SouthID = southRoom.RoomId;
+            }
+            
+            Room westRoom = roomContext.Rooms.Where(r => r.X == room.X - 1 && r.Y == room.Y).FirstOrDefault();
+            ViewBag.West = westRoom != null;
+            if(westRoom != null)
+            {
+                ViewBag.WestID = westRoom.RoomId;
+            }
+            
+            Room eastRoom = roomContext.Rooms.Where(r => r.X == room.X + 1 && r.Y == room.Y).FirstOrDefault();
+            ViewBag.East = eastRoom != null;
+            if(eastRoom != null)
+            {
+                ViewBag.EastID = eastRoom.RoomId;
+            }
+            
+            return View(room);
+        }
+
+        public IActionResult Move(int id)
+        {
+            Room targetRoom = roomContext.Rooms.Where(r => r.RoomId == id).FirstOrDefault();
+            return RedirectToAction("Index", targetRoom);
+        }
+
+    }
+}
---
+++ Unit 3/Module 11/M11Assignment/M11Assignment/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M11Assignment.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int X { get; set; }
+        [Required]
+        public int Y { get; set; }
+    }
+}
